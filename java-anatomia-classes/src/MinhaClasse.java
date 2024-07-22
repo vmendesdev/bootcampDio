c3738650888b73042f93315be2c8637dd11e01bf
Vanessa Mendes <vmendesjob@gmail.com>
2024-07-22 07:43:22 -0300

    Anatomia das Classes - 2

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -2,6 +2,18 @@ public class MinhaClasse {
     
     public static void main (String [] args) {
 
+        //Anatomia da Classe - 1
         System.out.print("Hello Wolrd!");
+
+        //Anatomia da Classe - 2
+        //Padrão Camel Case
+        int ano = 2021;
+        ano = 2022;
+        final String BR = "Brasil";
+
+        BR = "Brazil";
+
+        double PI = 3.14;
+
     }
 }
