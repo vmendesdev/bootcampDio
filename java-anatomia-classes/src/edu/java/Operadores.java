d694493d9984b092aa21efd32a16a7378c631116
Vanessa Mendes <vmendesjob@gmail.com>
2024-07-29 06:40:05 -0300

    praticando exemplos de operadores

---
+++ java-anatomia-classes/src/edu/java/Operadores.java
@@ -0,0 +1,26 @@
+public class Operadores {
+
+    //Realizar uma pesquisa de operadores 
+    String nome = "Vanessa";//entre aspas
+    char sexo = 'F'; //aspas simples 
+    Date dataNascimento = new Date(); //entre parenteses
+
+    double soma = 10.5 + 15.7;
+    int subtracao = 113 - 25;
+    int multiplicacao = 15 / 3;
+    int divisao = 15 / 3;
+    int modulo 18 % 3;
+    double resultado = (10 * 7) + (20 / 4);
+
+    //exemplos de concatenação
+
+    String concatenação = "?"; cacatenação = "1111"
+    System.out.println(concatenação); 
+
+    //++ operador unário de incremente de valor 
+    //-- operador unário de decremento de valor 
+    //! Operador unário de lógica de negação 
+
+    
+}
+ 
---
+++ java-anatomia-classes/src/edu/java/basico/Operadores2.java
@@ -0,0 +1,12 @@
+public class Operadores2 {
+
+    public static void main(String[] args) {
+        
+        int numero = 5;
+        //repetição
+        numero ++;
+        //numero + numero = 1;
+
+        System.out.println(numero);
+    }
+}
